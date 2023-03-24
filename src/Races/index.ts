ffe81c88c307cecbc9f9197925ad79f23d5c397e
Beatriz <beatriz.mediatto2@gmail.com>
2023-03-24 17:00:35 -0300

    feat: add class Orc

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -1,5 +1,8 @@
 import Race from './Race';
 import Dwarf from './Dwarf';
 import Elf from './Elf';
+import Halfling from './Halfling';
+import Orc from './Orc';
 
-export default { Race, Dwarf, Elf };
+export { Race, Dwarf, Elf, Halfling, Orc };
+export default Race;
